1af8952e77d05519ee879e5d7ccabf339f66ef7b
Ivan Tabarino <tabarino@outlook.com>
2021-08-11 23:06:38 +0100

    Create Use Case to Save Purchases

---
+++ src/domain/usecases/ISavePurchases.ts
@@ -0,0 +1,11 @@
+export interface ISavePurchases {
+  save: (purchases: Array<SavePurchases.Params>) => Promise<void>;
+}
+
+namespace SavePurchases {
+  export type Params = {
+    id: string,
+    date: Date,
+    value: number
+  }
+}
