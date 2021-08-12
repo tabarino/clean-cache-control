dd60e9729ca638df2ceff853602f0d757f69e619
Ivan Tabarino <tabarino@outlook.com>
2021-08-12 22:49:46 +0100

    Ensure LocalSavePurchases does not delete cache upon creation

--- src/domain/data/usecases/LocalSavePurchases.spec.ts
+++ src/domain/data/usecases/LocalSavePurchases.spec.ts
@@ -1,5 +1,18 @@
-describe('LocalSavePurchases', () => {
-  test('', () => {
-    expect(1).toBe(1);
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: ICacheStore) {}
+}
+
+interface ICacheStore {
+}
+
+class CacheStoreSpy implements ICacheStore {
+  deleteCallsCount = 0;
+}
+
+describe("LocalSavePurchases", () => {
+  test("Should not delete cache on Init", () => {
+    const cacheStore = new CacheStoreSpy();
+    const sut = new LocalSavePurchases(cacheStore); // sut - System Under Test
+    expect(cacheStore.deleteCallsCount).toBe(0);
   });
 });
